0a173d63ba2c2f6e8c423707ccd0d6e16604a46b
Admin <Admin@FOREVEREST>
2022-09-26 19:24:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,33 @@
+﻿double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+
+if (y > 0)
+{
+    if ((x >= 0) & (x <= 1) & (y <= 1))
+    {
+        Console.WriteLine("Exists");
+    }
+    else if ((Math.Abs(x) + Math.Abs(y) <= 1) & (x >= -1))
+    {
+        Console.WriteLine("Exists");
+    }
+    else
+    {
+        Console.WriteLine("Doesn't exists");
+    }
+}
+else if (y <= 0)
+{
+    if ((x >= -1) & (y * y + x * x <= 1) & (x <= 0))
+    {
+        Console.WriteLine("Exists");
+    }
+    else
+    {
+        Console.WriteLine("Doesn't exists");
+    }
+}
+else
+{
+    Console.WriteLine("Doesn't exists");
+}
